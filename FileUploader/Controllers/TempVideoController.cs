2a026cf0f0fd4b1886dead8e4055da6cda251630
Abir <abir@primetechbd.com>
2024-09-24 18:55:51 +0600

    Update TempVideoController.cs

--- FileUploader/Controllers/TempVideoController.cs
+++ FileUploader/Controllers/TempVideoController.cs
@@ -52,3 +52,120 @@
 //        return contentType;
 //    }
 //}
+
+//=======================
+
+
+//<!DOCTYPE html>
+//<html lang="en">
+
+//<head>
+//    <meta charset="UTF-8">
+//    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//    <title>Document</title>
+//</head>
+
+//<body>
+
+//    <script src="https://cdn.fluidplayer.com/v3/current/fluidplayer.min.js"></script>
+//    <!-- height="360" width="640" -->
+//     <!-- Avatar The Last Airbender S01E01.mp4 -->
+//    <!-- Wednesday S01E02  1080p NF WEBRip x265 HEVC MSubs [Dual Audio][Hindi 5.1+English 5.1] -OlaM.mkv -->
+//    <div style="height: 360px; width: 640px;">
+//        <video id="video-id">
+//            <source
+//                src="https://localhost:7001/api/filedownload/download/Avatar The Last Airbender S01E01.mp4"
+//                type="video/mp4" />
+//        </video>
+//    </div>
+
+//    <script>
+//        var myFP = fluidPlayer(
+//            'video-id', {
+//            "layoutControls": {
+//                "controlBar": {
+//                    "autoHideTimeout": 3,
+//                    "animated": true,
+//                    "autoHide": true
+//                },
+//                "htmlOnPauseBlock": {
+//                    "html": null,
+//                    "height": null,
+//                    "width": null
+//                },
+//                "autoPlay": false,
+//                "mute": true,
+//                "allowTheatre": true,
+//                "playPauseAnimation": false,
+//                "playbackRateEnabled": false,
+//                "allowDownload": false,
+//                "playButtonShowing": false,
+//                "fillToContainer": true,
+//                "posterImage": ""
+//            },
+//            "vastOptions": {
+//                "adList": [],
+//                "adCTAText": false,
+//                "adCTATextPosition": ""
+//            }
+//        });
+//    </script>
+//</body>
+
+//</html><!DOCTYPE html>
+//<html lang="en">
+
+//<head>
+//    <meta charset="UTF-8">
+//    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//    <title>Document</title>
+//</head>
+
+//<body>
+
+//    <script src="https://cdn.fluidplayer.com/v3/current/fluidplayer.min.js"></script>
+//    <!-- height="360" width="640" -->
+//     <!-- Avatar The Last Airbender S01E01.mp4 -->
+//    <!-- Wednesday S01E02  1080p NF WEBRip x265 HEVC MSubs [Dual Audio][Hindi 5.1+English 5.1] -OlaM.mkv -->
+//    <div style="height: 360px; width: 640px;">
+//        <video id="video-id">
+//            <source
+//                src="https://localhost:7001/api/filedownload/download/Avatar The Last Airbender S01E01.mp4"
+//                type="video/mp4" />
+//        </video>
+//    </div>
+
+//    <script>
+//        var myFP = fluidPlayer(
+//            'video-id', {
+//            "layoutControls": {
+//                "controlBar": {
+//                    "autoHideTimeout": 3,
+//                    "animated": true,
+//                    "autoHide": true
+//                },
+//                "htmlOnPauseBlock": {
+//                    "html": null,
+//                    "height": null,
+//                    "width": null
+//                },
+//                "autoPlay": false,
+//                "mute": true,
+//                "allowTheatre": true,
+//                "playPauseAnimation": false,
+//                "playbackRateEnabled": false,
+//                "allowDownload": false,
+//                "playButtonShowing": false,
+//                "fillToContainer": true,
+//                "posterImage": ""
+//            },
+//            "vastOptions": {
+//                "adList": [],
+//                "adCTAText": false,
+//                "adCTATextPosition": ""
+//            }
+//        });
+//    </script>
+//</body>
+
+//</html>
